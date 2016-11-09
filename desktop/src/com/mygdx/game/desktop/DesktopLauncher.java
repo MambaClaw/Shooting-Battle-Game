5ff6b0c4133cd7721914cfc8b2442bc86aa5859c
MambaClaw <nickidaiki@gmail.com>
2016-11-09 15:10:04 +0700

    Creat Game Field

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -6,7 +6,9 @@ import com.mygdx.game.ShootingGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ShootingGame(), config);
-	}
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new ShootingGame(), config);
+    }
 }
